1ba02d8ee2736900269f83b8b3ac3065072e9e07
George Kastanas <kastgeo@gmail.com>
2024-02-01 18:46:21 +0200

    YodiDesk: v2.3.0+60.21

--- YodiDeskWeb/flutter_service_worker.js
+++ YodiDeskWeb/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "bebdad435ff63c6cab76cdcc1ed043a6",
-"index.html": "0661e22782b7ebe83aee543379662e01",
-"/": "0661e22782b7ebe83aee543379662e01",
-"main.dart.js": "13bf167c2e902e7be923b61fb0ec7eac",
+"index.html": "ba1259d3773a0ba40f5f0be06b2bc0e2",
+"/": "ba1259d3773a0ba40f5f0be06b2bc0e2",
+"main.dart.js": "4e90e535c153d34ac782b4bd9ae9fce3",
 "signin_callback.html": "2bcdb79a8230fbf01b0df24824c97d6c",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "fdb560b353aa82926cb0fc1b1c358df5",
