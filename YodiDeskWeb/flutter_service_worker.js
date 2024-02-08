a1ff6447f30a4a89ad8b3488281e2607fa41fc36
George Kastanas <kastgeo@gmail.com>
2024-02-08 17:01:44 +0200

    YodiDesk: v2.3.0+60.25 use canvaskit

--- YodiDeskWeb/flutter_service_worker.js
+++ YodiDeskWeb/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "bebdad435ff63c6cab76cdcc1ed043a6",
-"index.html": "b9e2255c53835259e298e6a9022cbaa0",
-"/": "b9e2255c53835259e298e6a9022cbaa0",
-"main.dart.js": "ca301e7792c698d2d590fc2ce6d79046",
+"index.html": "8510d93de266e6cf3a53e141a3fc4992",
+"/": "8510d93de266e6cf3a53e141a3fc4992",
+"main.dart.js": "3291dbff3a217f871468a95000c6686e",
 "signin_callback.html": "2bcdb79a8230fbf01b0df24824c97d6c",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "fdb560b353aa82926cb0fc1b1c358df5",
